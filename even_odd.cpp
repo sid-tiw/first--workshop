8203f2ce0598c3d9f2bbed086208e6d545855746
sid-tiw <201851127@iiitvadodara.ac.in>
2019-04-12 15:03:04 +0530

    even_odd code

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number to check for : " ;
+    cin >> n;
+    if(n%2 ==0)
+        cout << "Hehe, the number you entered is even" << endl;
+    else
+        cout << "Tu chutiya hai. The number is odd!" << endl;
+    return 0;
+}
